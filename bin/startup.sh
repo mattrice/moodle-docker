2b28be185c21e2d268f790b4529b0eb43ab1a7cd
Matt Rice <3780536+mattrice@users.noreply.github.com>
2022-05-19 10:14:13 -0400

    Add some control flow and support VNC port

--- bin/startup.sh
+++ bin/startup.sh
@@ -7,13 +7,14 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
   cat <<EOF # remove the space between << and EOF, this is due to web plugin issue
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] [-p project_name] --code_path /path/to/moodle/code webport
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-i] [--dbengine mysql] [--dbport 3306] [-p project_name] [--vncport 5900] --code_path /path/to/moodle/code webport
 
-Script description here.
+Start a suite of Docker containers to support local development. Requires docker installed and running.
 
 Arguments:
 
-webport         Host port number to bind to the web server; for accessing the web server via a browser on the host (e.g. https://localhost:8000)
+webport         Host port number to bind to the web server.
+                For accessing the web server via a browser on the host (e.g. https://localhost:8000)
                 Required, commonly used value of 8000
 
 Available options:
@@ -22,12 +23,25 @@ Available options:
 -v, --verbose   Print script debug info
 -i, --install   Run the CLI install script
 --code_path     Path to the directory with the Moodle PHP code.
-                Required, typical value of /var/work/moodle
---dbengine      Which DB engine to spin up. Currently supported: pgsql, mariadb, mssql, mysql, oracle
-                Optional, default value of `mysql`.
---dbport        Host port number to bind to the database (default none); for connecting database tools on the host to the database container
-                Optional, typical value of 3306
--p, --project   Docker container prefix (default: docker_moodle), useful when spinning up multiple sets of containers
+                Required, typical value of '/var/work/moodle'
+--dbengine      Which DB engine to spin up.
+                Optional, default value of 'mysql'
+                Currently supported:
+                    pgsql
+                    mariadb
+                    mssql
+                    mysql
+                    oracle
+--dbport        Port number on the host bind to the database.
+                Useful for connecting database tools on the host to the database container.
+                NOTE: No port is mapped by default.
+                Optional, typical value of '3306'
+-p, --project   Docker container prefix, useful when spinning up multiple sets of containers.
+                Optional, default value of 'docker_moodle'
+--vncport       Port number on the host bind to for VNC.
+                Useful for viewing Behat tests from the host as the test run in the selenium container.
+                NOTE: No port is mapped by default.
+                Optional, typical value of '5900'
 EOF
   exit
 }
@@ -62,6 +76,7 @@ parse_params() {
   code_path=''
   dbengine='mysql'
   dbport=''
+  vncport=''
   project=''
   docker_path='/var/work/docker-moodle/'
 
@@ -85,6 +100,10 @@ parse_params() {
       ;;
     -p | --project)
       project="${2-}"
+      shift
+      ;;
+    --vncport) # long parameter only
+      vncport="${2-}"
       shift
       ;;
     -?*) die "Unknown option: $1" ;;
@@ -116,6 +135,7 @@ validate_options() {
   # - dbport
   #   - a number
   #   - greater than 1000
+  dbport=13306
   # - project
   #   - non-empty
   #   - no internal whitespace
@@ -138,17 +158,28 @@ xdebug.log=/var/log/xdebug.log
 EOF
 }
 
+
 parse_params "$@"
 validate_options
 setup_colors
 
+# Check for running Docker daemon
+docker ps > /dev/null
+if [ 0 -ne "$?" ]
+then
+  die "Docker does not appear to be running; aborting startup"
+fi
+
 CODE_DIR=/var/work/moodle
 DOCKER_DIR=/var/work/docker-moodle/
 
-export COMPOSE_PROJECT_NAME=moodle
+export COMPOSE_PROJECT_NAME=docker-moodle
 export MOODLE_DOCKER_WEB_PORT="$webport"
+export MOODLE_DOCKER_SELENIUM_VNC_PORT="$vncport"
 export MOODLE_DOCKER_WWWROOT="$CODE_DIR"
 export MOODLE_DOCKER_DB=mysql
+export MOODLE_DOCKER_DB_PORT="$dbport"
+
 cd "$DOCKER_DIR"
 cp config.docker-template.php "$MOODLE_DOCKER_WWWROOT"/config.php
 msg "Starting containers..."
@@ -157,37 +188,48 @@ bin/moodle-docker-compose up -d
 msg "Waiting for database to start..."
 bin/moodle-docker-wait-for-db
 
-msg "Installing XDebug from PECL..."
-# If pecl fails to install xdebug that's OK
-bin/moodle-docker-compose exec webserver pecl install xdebug ||true
+# Check for installed PHP modules
+xdebug=$(bin/moodle-docker-compose exec webserver bash -c "php -m | grep xdebug")
+if [ "xdebug" -ne "$xdebug" ]
+then
+  msg "Installing XDebug from PECL..."
+  # If pecl fails to install xdebug that's OK
+  bin/moodle-docker-compose exec webserver pecl install xdebug ||true
 
-msg "Generating and injecting XDebug config..."
-# Yes, we are blindly overwriting any existent XDebug config
-bin/moodle-docker-compose exec webserver bash -c "echo '$(xdebug_config)' > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
+  msg "Generating and injecting XDebug config..."
+  # Yes, we are blindly overwriting any existent XDebug config
+  bin/moodle-docker-compose exec webserver bash -c "echo '$(xdebug_config)' > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
+fi
 
+# Even if the XDebug extension was installed, it might not have been enabled
 msg "Enabling XDebug and restarting webserver..."
-bin/moodle-docker-compose exec webserver docker-php-ext-enable xdebug
+# If XDebug is already loaded, the following call will emit a warning (that we will suppress)
+bin/moodle-docker-compose exec webserver docker-php-ext-enable xdebug > /dev/null
 bin/moodle-docker-compose restart webserver
 
-# TODO - run install script, if asked
-#   The CLI script appears to detect existing installation, so always running this is not the worst thing ever
-# Initialize Moodle database for manual testing
-msg "Running init scripts..."
-bin/moodle-docker-compose exec webserver php admin/cli/install_database.php \
-  --agree-license \
-  --fullname="Docker moodle" \
-  --shortname="docker_moodle" \
-  --summary="Docker moodle site" \
-  --adminpass="test" \
-  --adminemail="admin@example.com" \
-  ||true
+
+if [ "1" -eq "$install" ]
+then
+  #   The CLI script appears to detect existing installation, so always running this is not the worst thing ever
+  # Initialize Moodle database for manual testing
+  msg "Running init scripts..."
+  bin/moodle-docker-compose exec webserver php admin/cli/install_database.php \
+    --agree-license \
+    --fullname="Docker moodle" \
+    --shortname="docker_moodle" \
+    --summary="Docker moodle site" \
+    --adminpass="test" \
+    --adminemail="admin@example.com" \
+    ||true
+fi
 
 msg "${GREEN}Local environment started${NOFORMAT}. Changes made in ${BLUE}${MOODLE_DOCKER_WWWROOT}${NOFORMAT} will be reflected live at ${BLUE}http://localhost:${webport}${NOFORMAT}"
 
-msg "${RED}Read parameters:${NOFORMAT}"
+msg "${RED}Parsed/used parameter values:${NOFORMAT}"
 msg "- install: ${install}"
 msg "- code_path: ${code_path}"
 msg "- dbengine: ${dbengine}"
 msg "- dbport: ${dbport}"
 msg "- project: ${project}"
+msg "- vncport: ${vncport}"
 msg "- arguments: ${args[*]-}"
